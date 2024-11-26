c9d1a4666e18fd1b0cf836a3071d4a0ba621a13e
Samuel Lucas <samuel.lucasmacedos@gmail.com>
2024-11-26 10:24:23 -0300

    update2

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -8,7 +8,7 @@ namespace MeuTodo.Models
         public int Id { get; set; }
         public string Title { get; set; } 
         // Esse é o campo required no view model que indica que esse campo não pode ser vazio. Será enviado pelo usuário, diferente das outras prop.
-        public bool Done { get; set; }
+        public bool Done { get; set; } = false;
         public DateTime Date { get; set; } = DateTime.Now; // Caso a data não seja especificada, será usado a data de quando ele enviou o dado pra API.
     }
 }
